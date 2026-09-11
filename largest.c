16f40e42ff52a82d83db89470d6fe1cd0a8065d0
4rshiq <4rshiq007@gmail.com>
2026-09-11 20:10:19 +0530

    added logic

--- largest.c
+++ largest.c
@@ -1,8 +1,24 @@
 #include<stdio.h>
 int main()
 {
-    int n;
-    printf("enter the length of your array");
+    int n,largest;
+    printf("enter the length of your array: ");
+    scanf("%d",&n);
     int array[n];
+    printf("enter the elements of your array");
+    for (int i = 0; i < n; i++)
+    {
+            scanf("%d",&array[i]);
+    }
+    largest = array[0];
+    for (int i = 0; i < n; i++)
+    {
+            if (array[i]>largest)
+            {
+                largest = array[i];
+            }
+    }
+    printf("%d is the largest element in the array\n",largest);
+    
     return 0;
 }
